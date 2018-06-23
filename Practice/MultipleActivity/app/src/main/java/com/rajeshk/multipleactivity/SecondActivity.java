be6cea8ff0e514ec2294abdac46127ad9d43686c
rajeshranwa <rajeshranwa94@gmail.com>
2018-06-23 22:20:52 +0530

    added MultipleActivity demo app

---
+++ Practice/MultipleActivity/app/src/main/java/com/rajeshk/multipleactivity/SecondActivity.java
@@ -0,0 +1,39 @@
+package com.rajeshk.multipleactivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        /*fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });*/
+
+        Intent intent = getIntent();
+        Toast.makeText(this,  intent.getStringExtra("name"), Toast.LENGTH_LONG).show();
+
+    }
+    public void backButtonOnClick(View view){
+        Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+        startActivity(intent);
+    }
+
+}
